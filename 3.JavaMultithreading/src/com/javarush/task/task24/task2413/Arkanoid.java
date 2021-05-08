9e248f05b27ba3195c8f236119f878bb82e86a9f
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-05-08 13:27:17 +0300

    Add Game Arkanoid, task 2413

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task24.task2413;
+
+public class Arkanoid {
+    private int width;
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public Arkanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
