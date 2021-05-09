faf18ca9627f8ddcd51c11f5b717482a8513123d
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-05-10 00:10:19 +0300

    Update 3 Game Arkanoid, task 2413

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task24.task2413;
 
-public class Ball extends BaseObject{
+public class Ball extends BaseObject {
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
     }
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task24.task2413;
 
-public class Brick extends BaseObject{
+public class Brick extends BaseObject {
     public Brick(double x, double y, double radius) {
         super(x, y, radius);
     }
