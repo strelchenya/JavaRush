409f650dddb194fc7ee7fc80ba10e73bdcfc590d
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-05-15 20:55:43 +0300

    Add task 3102 fix return path 2

--- 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -49,4 +49,28 @@ public class Solution {
             e.printStackTrace();
         }
     }
+
+    /*public static List<String> getFileTree(String root) throws IOException {
+        List<String> result = new ArrayList<>();
+
+        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(Paths.get(root), options, 20, new GetFiles(result));
+
+        return result;
+    }
+
+    private static class GetFiles extends SimpleFileVisitor<Path> {
+        private List<String> result;
+
+        public GetFiles(List<String> result) {
+            this.result = result;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+            String s = path.toAbsolutePath().toString();
+            result.add(s);
+            return FileVisitResult.CONTINUE;
+        }
+    }*/
 }
