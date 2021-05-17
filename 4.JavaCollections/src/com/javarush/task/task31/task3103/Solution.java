ee7b0f4e6e0e7f46ba95e5d476385aedaebfbd41
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-05-17 17:35:06 +0300

    Add task 3103 fix Charset

--- 4.JavaCollections/src/com/javarush/task/task31/task3103/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3103/Solution.java
@@ -11,17 +11,24 @@ import java.util.List;
 */
 
 public class Solution {
+
+    //readBytes - должен возвращать все байты файла fileName
     public static byte[] readBytes(String fileName) throws IOException {
-        return null;
+        return Files.readAllBytes(Paths.get(fileName));
     }
 
+    //readLines - должен возвращать все строки файла fileName. Используй кодировку по умолчанию.
     public static List<String> readLines(String fileName) throws IOException {
-        return null;
+        return Files.readAllLines(Paths.get(fileName), Charset.defaultCharset());
     }
 
+    //writeBytes - должен записывать массив bytes в файл fileName
     public static void writeBytes(String fileName, byte[] bytes) throws IOException {
+        Files.write(Paths.get(fileName), bytes);
     }
 
+    //copy - должен копировать файл resourceFileName на место destinationFileName
     public static void copy(String resourceFileName, String destinationFileName) throws IOException {
+        Files.copy(Paths.get(resourceFileName), Paths.get(destinationFileName));
     }
 }
