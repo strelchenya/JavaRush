393e9e12a10c1086d8439966758314bd854ce794
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-05-18 18:14:34 +0300

    Add task 3113-2

--- 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -6,11 +6,54 @@ import java.io.InputStreamReader;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.EnumSet;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /* 
 Что внутри папки?
 */
 
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String dirName = reader.readLine();
+
+        Path directory = Paths.get(dirName);
+
+        if (!Files.isDirectory(directory)) {
+            System.out.println(directory.toString() + " - не папка.");
+        } else {
+            AtomicInteger folderCounter = new AtomicInteger();
+            AtomicInteger fileCounter = new AtomicInteger();
+            AtomicLong sizeCounter = new AtomicLong();
+
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    if (!dir.equals(directory)) folderCounter.incrementAndGet();
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    fileCounter.incrementAndGet();
+                    sizeCounter.addAndGet(attrs.size());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            System.out.println("Параметры папки " + directory.toString() + ":");
+            System.out.println("Всего папок - " + folderCounter.get());
+            System.out.println("Всего файлов - " + fileCounter.get());
+            System.out.println("Общий размер - " + sizeCounter.get() + " байт");
+        }
+    }
+}
+
+
+
+/* -------------------------Первый вариант------------------------
 public class Solution extends SimpleFileVisitor<Path>{
 
 public static int countFile = 0;
@@ -58,4 +101,4 @@ public static double heightFile = 0;
         heightFile += attrs.size();
         return FileVisitResult.CONTINUE;
     }
-}
+}*/
