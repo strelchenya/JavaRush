9bae4caa79127fee6168d79f83bcf296861e4c23
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-05-22 23:00:46 +0300

    Add task 2028 fix methods add

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,15 +1,55 @@
 package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /* 
 Построй дерево(1)
 */
 
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+    //    List<Entry<String>> listEntry = new LinkedList<>();
+    Entry<String> root;
+    //    static List<Entry<String>> listEntry = new ArrayList<>();
+    int countEntry = 0;
+
+    public CustomTree() {
+        this.root = new Entry("0");
+        root.parent = root;
+    }
+
+    @Override
+    public boolean add(String s) {
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.add(root);
+
+        Entry<String> entrySting = new Entry<>(s);
+        while (!queue.isEmpty()) {
+            Entry<String> entry = queue.poll();
+            if (entry.isAvailableToAddChildren()) {
+                if (entry.availableToAddLeftChildren) {
+                    entry.leftChild = entrySting;
+                    entry.availableToAddLeftChildren = false;
+                    entrySting.parent = entry;
+//                    listEntry.add(entrySting);
+                    countEntry++;
+                    return true;
+                }
+                if (entry.availableToAddRightChildren) {
+                    entry.rightChild = entrySting;
+                    entry.availableToAddRightChildren = false;
+                    entrySting.parent = entry;
+                    countEntry++;
+                    return true;
+                }
+            }
+
+        }
+
+
+        return false;
+    }
+
     @Override
     public String get(int index) {
         throw new UnsupportedOperationException();
@@ -47,7 +87,13 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
     @Override
     public int size() {
-        return 0;
+
+        return this.countEntry;
+    }
+
+    public String getParent(String s) {
+
+        return "1";
     }
 
     static class Entry<T> implements Serializable {
@@ -62,9 +108,16 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
             this.availableToAddRightChildren = true;
         }
 
+        public Entry(String elementName, Entry leftChild, Entry rightChild) {
+            this.elementName = elementName;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+        }
+
         public boolean isAvailableToAddChildren() {
             return availableToAddLeftChildren || availableToAddRightChildren;
 
         }
+
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -13,5 +13,8 @@ public class Solution {
         System.out.println("The list size is " + list.size());
         System.out.println("The expected parent is 3. The actual parent is " + ((CustomTree) list).getParent("8"));
         System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
+        System.out.println("size:" + list.size() + "; removing 14: " + list.remove("14") + "; size after:" + list.size());
+        System.out.println("size:" + list.size() + "; removing 2: " + list.remove("2") + "; size after:" + list.size());
+        System.out.println("size:" + list.size() + "; removing 2: " + list.remove("5") + "; size after:" + list.size());
     }
 }
