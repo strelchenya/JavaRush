a12f59a6a9bc9c76dc49d7ea6b4fe9cc31b4d7b4
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-05-26 00:30:44 +0300

    Add task 3204 fix getPassword

--- 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
@@ -15,43 +15,73 @@ public class Solution {
 
     public static ByteArrayOutputStream getPassword() throws IOException {
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
 
         byte[] base = new byte[10 + 26 + 26];
         int count = 0;
         for (byte i = 48; i <= 122; i++) {
+            //диапазон в цифр
             if (i >= 48 && i <= 57) {
                 base[count++] = i;
+                //диапазон A-Z
             } else if (i >= 65 && i <= 90) {
                 base[count++] = i;
+                //диапазон a-z
             } else if (i >= 97 && i <= 122) {
                 base[count++] = i;
             }
         }
 
-        byte[] passwordRandom = getRandom(base);
+        String passString = "";
+        byte[] passwordRandom;
 
-        System.out.println(new String(passwordRandom));
+        do {
+            passwordRandom = new byte[8];
+
+            for (int i = 0; i < passwordRandom.length; i++) {
+                int minRandom = 0;
+                int maxRandom = base.length - 1;
+                maxRandom -= minRandom;
+                int randomNumber = (int) (Math.random() * ++maxRandom) + minRandom;
+                passwordRandom[i] = base[randomNumber];
+            }
+
+            passString = new String(passwordRandom);
+        }
+        //проверки на наличие символов
+        while (!passString.matches(".*[A-Z].*") ||
+                !passString.matches(".*[a-z].*") ||
+                !passString.matches(".*\\d.*"));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
         baos.write(passwordRandom);
 
         return baos;
     }
+}
 
-
+/*
     public static byte[] getRandom (byte[] arrayBase){
+        byte[] pass = arrayBase;
+
         byte[] passwordRandom = new byte[8];
 
         for (int i = 0; i < passwordRandom.length; i++) {
             int minRandom = 0;
             int maxRandom = arrayBase.length;
             maxRandom -= minRandom;
-            int randomNamber = (int) (Math.random() * ++maxRandom) + minRandom;
-            passwordRandom[i] = arrayBase[randomNamber];
+            int randomNumber = (int) (Math.random() * ++maxRandom) + minRandom;
+            passwordRandom[i] = arrayBase[randomNumber];
         }
 
-        //проверить на содержание одной цифры, на букву маленькую и большую
+        String passString = new String(passwordRandom);
+
+        if (!passString.matches(".*[A-Z].*")) return getRandom(pass);
+
+        if (!passString.matches(".*[a-z].*")) return getRandom(pass);
+
+        if (!passString.matches(".*\\d.*")) return getRandom(pass);
 
         return passwordRandom;
 
-    }
-}
+    }*/
+
