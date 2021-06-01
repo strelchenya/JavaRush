a4734c4709154143c9f2a805b9bca9cf7ccb9db4
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-01 13:59:57 +0300

    Add task 3302 fix class Cat

--- 4.JavaCollections/src/com/javarush/task/task33/task3302/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3302/Solution.java
@@ -31,10 +31,13 @@ public class Solution {
 
     @JsonAutoDetect
     public static class Cat {
+
+        @JsonProperty("wildAnimal")
         public String name;
 
+        @JsonIgnore
         public int age;
-
+        @JsonProperty("over")
         public int weight;
 
         Cat() {
