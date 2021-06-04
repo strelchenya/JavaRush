ce015da6c30cb93f44c6f8fedd3f3ddf6baf6e5f
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-04 08:41:27 +0300

    Add task 3307 add method convertFromXmlToNormal

--- 4.JavaCollections/src/com/javarush/task/task33/task3307/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3307/Solution.java
@@ -20,7 +20,12 @@ public class Solution {
     }
 
     public static <T> T convertFromXmlToNormal(String xmlData, Class<T> clazz) throws IOException, JAXBException {
-        return null;
+        StringReader reader = new StringReader(xmlData);
+
+        JAXBContext context = JAXBContext.newInstance(clazz);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        return (T) unmarshaller.unmarshal(reader);
     }
 
     @XmlType(name = "cat")
