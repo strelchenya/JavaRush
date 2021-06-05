0e43ed3fcc3e117422df1a363813cca36bbb4341
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-05 15:55:21 +0300

    Add task 3309 add DOMSource

--- 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -1,19 +1,27 @@
 package com.javarush.task.task33.task3309;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -24,25 +32,93 @@ import java.util.List;
 */
 
 public class Solution {
-    public static String toXmlWithComment(Object obj, String tagName, String comment) throws JAXBException {
+    public static String toXmlWithComment(Object obj, String tagName, String comment) {
 
         //писать результат сериализации будем в Writer(StringWriter)
         StringWriter writer = new StringWriter();
 
         //создание объекта Marshaller, который выполняет сериализацию
-        JAXBContext context = JAXBContext.newInstance(First.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        JAXBContext context = null;
+        try {
+            context = JAXBContext.newInstance(obj.getClass());
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        Marshaller marshaller = null;
+        try {
+            marshaller = context.createMarshaller();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        try {
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        } catch (PropertyException e) {
+            e.printStackTrace();
+        }
         // сама сериализация
-        marshaller.marshal(obj, writer);
+        try {
+            marshaller.marshal(obj, writer);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
 
+        InputStream stream = new ByteArrayInputStream(writer.toString().getBytes(StandardCharsets.UTF_8));
         //преобразовываем в строку все записанное в StringWriter
-        String result = writer.toString();
+       // String result = writer.toString();
 
-        return result;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        //проверяет, сконфигурирован ли DOM-анализатор для преобразования узлов типа CDATA в узлы типа Text
+        // и присоединяют их к соседнему узлу (если он существует) в документе XML
+        // или устанавливают режим такого преобразования в документе XML (при значении coalescing, равном true)
+        // По умолчанию значение coalescing установлено в false.)
+        dbf.setCoalescing(true);
+        DocumentBuilder db = null;
+        try {
+            db = dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        Document doc = null;
+        try {
+            doc = db.parse(stream);
+            doc.getDocumentElement().normalize();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        NodeList nodeList = doc.getElementsByTagName(tagName);
+
+        for (int s = 0; s < nodeList.getLength(); s++) {
+            Node node = nodeList.item(s);
+            if (node.getNodeName().equals(tagName)) {
+                node.getParentNode().insertBefore(doc.createComment(comment), node);
+                node.getParentNode().insertBefore(doc.createTextNode("\n"), node);
+            }
+        }
+
+        DOMSource source = new DOMSource(doc);
+        StringWriter sw = new StringWriter();
+        StreamResult result = new StreamResult(sw);
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = null;
+        try {
+            transformer = factory.newTransformer();
+
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty(OutputKeys.STANDALONE, "no");
+        transformer.transform(source, result);
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+        return sw.toString();
     }
 
-    public static void main(String[] args) throws JAXBException {
+    public static void main(String[] args)  {
         System.out.println(Solution.toXmlWithComment(new First(), "second", "it's a comment"));
     }
 
