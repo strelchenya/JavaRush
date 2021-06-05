929f6a88e6691c5d8898c7559bfd534a4d4ac085
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-05 13:45:20 +0300

    Add task 3309 parse to XML string

--- 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task33.task3309;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -23,12 +24,50 @@ import java.util.List;
 */
 
 public class Solution {
-    public static String toXmlWithComment(Object obj, String tagName, String comment) {
+    public static String toXmlWithComment(Object obj, String tagName, String comment) throws JAXBException {
 
-        return null;
+        //писать результат сериализации будем в Writer(StringWriter)
+        StringWriter writer = new StringWriter();
+
+        //создание объекта Marshaller, который выполняет сериализацию
+        JAXBContext context = JAXBContext.newInstance(First.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        // сама сериализация
+        marshaller.marshal(obj, writer);
+
+        //преобразовываем в строку все записанное в StringWriter
+        String result = writer.toString();
+
+        return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
+        System.out.println(Solution.toXmlWithComment(new First(), "second", "it's a comment"));
+    }
 
+    @XmlRootElement(name = "first")
+    public static class First {
+
+        @XmlElement(name = "second")
+        public String item1 = "some string";
+        @XmlElement(name = "second")
+        public String item2 = "need CDATA because of <second>";
+        @XmlElement(name = "second")
+        public String item3 = "";
+        @XmlElement(name = "third")
+        public String item4;
+        @XmlElement(name = "forth")
+        public Second[] third = new Second[]{new Second()};
+        @XmlElement(name = "fifth")
+        public String item5 = "need CDATA because of \"";
+    }
+
+    public static class Second {
+
+        @XmlElement(name = "second")
+        public String item1 = "some string";
+        @XmlElement(name = "second")
+        public String item2 = "need CDATA because of <second>";
     }
 }
