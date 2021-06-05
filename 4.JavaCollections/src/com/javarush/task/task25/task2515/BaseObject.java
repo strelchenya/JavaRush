b18a176575ddd646323d35275c1556f8f038ccd2
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-05 21:19:40 +0300

    Add task 2515-7 The game Space

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -1,21 +1,22 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Базовый класс для всех объектов игры.
+ */
 public abstract class BaseObject {
-
-    private double x;
-    private double y;
-    private double radius;
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
+    //состояние объект - жив ли объект
     private boolean isAlive;
 
     public BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        isAlive = true;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
+        this.isAlive = true;
     }
 
     public double getX() {
@@ -42,18 +43,46 @@ public abstract class BaseObject {
         this.radius = radius;
     }
 
-    public void draw(){
-
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    public void draw(Canvas canvas) {
+        //do nothing
     }
-    public void move(){
 
+    /**
+     * Двигаем себя на один ход.
+     */
+    public void move() {
+        //do nothing
     }
 
-    public void die(){
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    public void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public void die() {
         isAlive = false;
     }
 
-    public boolean isIntersect(BaseObject o){
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
+    public boolean isIntersect(BaseObject o) {
         double dx = x - o.x;
         double dy = y - o.y;
         double distance = Math.sqrt(dx * dx + dy * dy);
