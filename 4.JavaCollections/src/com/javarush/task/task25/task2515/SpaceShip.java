77b575e8cda7a0e25aab64cbbbdcea494a193292
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-06 10:53:08 +0300

    Add task 2515-13 The game Space

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -3,15 +3,34 @@ package com.javarush.task.task25.task2515;
 public class SpaceShip extends BaseObject {
 
     private double dx = 0;
+
     public SpaceShip(double x, double y) {
         super(x, y, 3);
     }
 
-    public void moveLeft(){
+    public void moveLeft() {
         dx = -1;
     }
 
-    public void moveRight(){
+    public void moveRight() {
         dx = 1;
     }
+
+    public void move() {
+
+        x += dx;
+        checkBorders(0, Space.game.getWidth(), 0, Space.game.getHeight());
+    }
+
+    public void draw(Canvas canvas) {
+    }
+
+
+    public void fire() {
+        Rocket rocketL = new Rocket(x - 2, y);
+        Rocket rocketR = new Rocket(x + 2, y);
+        Space.game.getRockets().add(rocketL);
+        Space.game.getRockets().add(rocketR);
+    }
+
 }
