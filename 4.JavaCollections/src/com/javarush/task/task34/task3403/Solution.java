446e772ec4cd98812be7e4d6dc5c89c5b84d128a
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-07 18:09:37 +0300

    Add task 3403 add method recurse

--- 4.JavaCollections/src/com/javarush/task/task34/task3403/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3403/Solution.java
@@ -6,5 +6,42 @@ package com.javarush.task.task34.task3403;
 
 public class Solution {
     public void recurse(int n) {
+        /*int i, j;
+
+        for (i = 2; i <= n; i++) {
+            int k = 0;
+
+            for (j = 2; j <= i; j++) {
+                if ((i % j) == 0)
+                    k++;
+            }
+            if (k < 2) {
+                if (n % i == 0) {
+                    *//*if (n == i) {
+                        System.out.print(i + " ");
+                        break;
+                    }*//*
+                    System.out.print(i + " ");
+                    recurse(n / i);
+                    break;
+                }
+            }
+
+        }*/
+
+
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                recurse(n / i);
+                break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.recurse(132);
     }
 }
