e071f06df99fa141ad3d421f7909202e67a9d25f
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-14 14:42:31 +0300

    Add task Game AmigoSet-3

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,25 +1,23 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
-public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
-    @Override
-    public int size() {
-        return 0;
+public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Serializable {
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        this.map = new HashMap<>();
     }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
+    public AmigoSet(Collection<? extends E> collection) {
+        this.map = new HashMap<>(Math.max((int) (collection.size() / .75f) + 1, 16));
+        addAll(collection);
     }
 
-    @Override
-    public boolean contains(Object o) {
-        return false;
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
     }
 
     @Override
@@ -28,48 +26,7 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
     }
 
     @Override
-    public Object[] toArray() {
-        return new Object[0];
+    public int size() {
+        return 0;
     }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(E e) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
 }
