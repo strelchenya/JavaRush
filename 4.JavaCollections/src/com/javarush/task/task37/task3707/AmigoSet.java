624bec7e8947b359e214c80d58efa8449b438cc1
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-14 17:05:30 +0300

    Add task Game AmigoSet-6

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,8 +1,9 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
-import java.util.function.Consumer;
 
 public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Serializable {
     private static final Object PRESENT = new Object();
@@ -23,8 +24,12 @@ public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Se
 
     @Override
     public Iterator<E> iterator() {
-        Iterator<E> iteratorMap = map.keySet().iterator();
-        return iteratorMap;
+        return map.keySet().iterator();
+    }
+
+    @Override
+    public int size() {
+        return map.size();
     }
 
     @Override
@@ -34,12 +39,7 @@ public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Se
 
     @Override
     public boolean contains(Object o) {
-        return map.keySet().contains(o);
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return map.keySet().remove(o);
+        return map.containsKey(o);
     }
 
     @Override
@@ -48,21 +48,54 @@ public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Se
     }
 
     @Override
-    public int size() {
-        return map.size();
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
     }
 
     @Override
-    public Object clone(){
-        AmigoSet<E> amigoSet = null;
+    public Object clone() {
         try {
-            amigoSet = new AmigoSet<>();
-            amigoSet.addAll(this);
-            amigoSet.map.putAll((Map) this.map.clone());
-        } catch (Exception e){
+            AmigoSet<E> newSet = (AmigoSet<E>) super.clone();
+            newSet.map = (HashMap<E, Object>) map.clone();
+            return newSet;
+        } catch (Exception e) {
             throw new InternalError();
         }
+    }
 
-        return amigoSet;
+    private void writeObject(ObjectOutputStream s) throws java.io.IOException {
+        // Write out any hidden serialization magic
+        s.defaultWriteObject();
+
+        // Write out HashMap capacity and load factor
+        s.writeInt(HashMapReflectionHelper.<Integer>callHiddenMethod(map, "capacity"));
+        s.writeFloat(HashMapReflectionHelper.<Float>callHiddenMethod(map, "loadFactor"));
+
+        // Write out size
+        s.writeInt(map.size());
+
+        // Write out all elements in the proper order.
+        for (E e : map.keySet())
+            s.writeObject(e);
+    }
+
+
+    private void readObject(ObjectInputStream s) throws java.io.IOException, ClassNotFoundException {
+        // Read in any hidden serialization magic
+        s.defaultReadObject();
+
+        // Read in HashMap capacity and load factor and create backing HashMap
+        int capacity = s.readInt();
+        float loadFactor = s.readFloat();
+        map = new HashMap<>(capacity, loadFactor);
+
+        // Read in size
+        int size = s.readInt();
+
+        // Read in all elements in the proper order.
+        for (int i = 0; i < size; i++) {
+            E e = (E) s.readObject();
+            map.put(e, PRESENT);
+        }
     }
 }
