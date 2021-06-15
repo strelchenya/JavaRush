b748b8b03f8c7466cd7e2f0059471ddb5653fbbe
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-15 12:24:17 +0300

    Add task 2404 add Inner Classes

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2404/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2404/Solution.java
@@ -7,11 +7,11 @@ package com.javarush.task.task24.task2404;
 public class Solution {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(1, 2, 3, 4);
-        System.out.println(getHeight(rectangle));
-        System.out.println(getWidth(rectangle));
+//        System.out.println(getHeight(rectangle));
+//        System.out.println(getWidth(rectangle));
         /////////////////////expected//////////////////
-        //System.out.println(getHeight(rectangle.castToHasHeight()));
-        //System.out.println(getWidth(rectangle.castToHasWidth()));
+        System.out.println(getHeight(rectangle.castToHasHeight()));
+        System.out.println(getWidth(rectangle.castToHasWidth()));
     }
 
     public static double getHeight(HasHeight rectangle) {
@@ -23,7 +23,7 @@ public class Solution {
     }
 
 
-    public static class Rectangle implements HasHeight, HasWidth {
+    public static class Rectangle /*implements HasHeight, HasWidth*/ {
         private Point point1;
         private Point point2;
 
@@ -32,12 +32,28 @@ public class Solution {
             point2 = new Point(x2, y2);
         }
 
-        public double getHeight() {
-            return Math.abs(point1.getY() - point2.getY());
+        public HasHeight castToHasHeight() {
+
+            class GetHeightClass implements HasHeight{
+
+                @Override
+                public double getHeight() {
+                    return Math.abs(point1.getY() - point2.getY());
+                }
+            }
+            return new GetHeightClass();
         }
 
-        public double getWidth() {
-            return Math.abs(point1.getX() - point2.getX());
+        public HasWidth castToHasWidth() {
+
+            class GetWidthClass implements HasWidth{
+
+                @Override
+                public double getWidth() {
+                    return Math.abs(point1.getX() - point2.getX());
+                }
+            }
+            return new GetWidthClass();
         }
     }
 }
