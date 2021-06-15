27eca43f57f50abf0967eb1fab35f6e332dbd78f
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-15 14:21:55 +0300

    Add task 2405 add Anonymous Inner Class

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2405/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2405/Solution.java
@@ -10,10 +10,41 @@ public class Solution implements Action {
     private int param;
 
     private Action solutionAction = new Action() {
-        //напишите тут ваш код
+        private FirstClass first;
+        private SecondClass second;
 
         public void someAction() {
-            //напишите тут ваш код
+            if (param > 0) {
+                if (first == null) {
+                    first = new FirstClass() {
+                        @Override
+                        public void someAction() {
+                            super.someAction();
+                            Solution.this.someAction();
+                        }
+
+                        @Override
+                        public Action getDependantAction() {
+                            System.out.println(param);
+                            param--;
+                            return param > 0 ? Solution.this : this;
+                        }
+                    };
+                }
+                first.getDependantAction().someAction();
+            } else {
+                if (second == null) {
+                    second = new SecondClass() {
+                        @Override
+                        public void someAction() {
+                            sb.append(SPECIFIC_ACTION_FOR_ANONYMOUS_SECOND_CLASS_PARAM).append(param);
+                            super.someAction();
+
+                        }
+                    };
+                }
+                second.someAction();
+            }
         }
     };
 
