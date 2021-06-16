69dc0f0206dad891250d4e634169e5808acbea80
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-16 15:00:55 +0300

    Add task 2411

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2411/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2411/Solution.java
@@ -4,7 +4,7 @@ package com.javarush.task.task24.task2411;
 Вспомним наследование
 */
 
-public class Solution {
+public class Solution extends C{
     private class A {
         protected String value = "A";
 
@@ -15,6 +15,7 @@ public class Solution {
 
     private A a = new A() {
         { //у анонимных классов нет своих конструкторов, но что-то можно сделать в блоке инициализации класса
+
             value = "Y";
             if (super.getClass().getName().contains(".Solution$")) {
                 System.out.print(value);
