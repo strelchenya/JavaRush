b5d293ade4a82e2a5109f4719a57d6c3f4bf5e99
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-19 19:01:25 +0300

    Add task 2506 add method run

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2506/LoggingStateThread.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task25.task2506;
+
+public class LoggingStateThread extends Thread{
+    private Thread thread;
+
+    public LoggingStateThread(Thread thread) {
+//        super(target);
+        this.thread = thread;
+    }
+
+    @Override
+    public void run() {
+        State currentState = thread.getState();
+        System.out.println(currentState);
+
+        State newState;
+        do {
+            if ((newState = thread.getState()) != currentState) {
+                currentState = newState;
+                System.out.println(newState);
+            }
+        } while (!currentState.equals(State.TERMINATED));
+    }
+}
