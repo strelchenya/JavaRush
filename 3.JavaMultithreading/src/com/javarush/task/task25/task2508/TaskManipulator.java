28e5e3f0ccdad49d2015dfa4018c291b83822f06
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-20 20:52:52 +0300

    Add task 2508 add Thread

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -1,7 +1,33 @@
 package com.javarush.task.task25.task2508;
 
-public class TaskManipulator implements Runnable {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
+
+    private Thread thread;
+
+    @Override
+    public void start(String threadName) {
+        //List<Thread> threads = new ArrayList<>();
+        thread = new Thread(this, threadName);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
+    }
+
     @Override
     public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println(Thread.currentThread().getName());
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+
+        }
     }
 }
