ef37f249455d54a468990419df3cbf5fc304b0d6
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-21 19:51:44 +0300

    Add task 2510

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -7,6 +7,16 @@ package com.javarush.task.task25.task2510;
 public class Solution extends Thread {
 
     public Solution() {
+        this.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                String message;
+                if (e instanceof Error) message = "Нельзя дальше работать";
+                else if (e instanceof Exception) message = "Надо обработать";
+                else message = "Поживем - увидим";
+                System.out.println(message);
+            }
+        });
     }
 
     public static void main(String[] args) {
