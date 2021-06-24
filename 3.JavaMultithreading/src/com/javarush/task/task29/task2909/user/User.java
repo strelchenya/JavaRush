a5f4030c934f2ea3a5d9569d79994bf5c400e06d
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-24 17:23:34 +0300

    Add task 2909 refactoring-15

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -5,16 +5,45 @@ public class User {
     private String surname;
     private int age;
 
-    private String country;
-    private String city;
-    private House house;
+    private Address address;
+
 
     private Work work;
+    private boolean man = true;
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
 
     public User(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
+    }
+
+
+    public String getCountry() {
+        return address.getCountry();
+    }
+
+    public void setCountry(String country) {
+        address.setCountry(country);
+    }
+
+    public String getCity() {
+        return address.getCity();
+    }
+
+    public void setCity(String city) {
+        address.setCity(city);
+    }
+
+    public String getAddress() {
+        return getCountry() + " " + getCity() + " " + address.getHouse();
     }
 
     public String getName() {
@@ -41,26 +70,6 @@ public class User {
         this.age = age;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getAddress() {
-        return country + " " + city + " " + house.house;
-    }
-
     public Work getWork() {
         return work;
     }
@@ -79,5 +88,9 @@ public class User {
             System.out.println("Пользователь моложе 16 лет");
         else
             System.out.println("Пользователь старше 16 лет");
+    }
+
+    public String getBoss(){
+       return work.getBoss();
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
@@ -7,9 +7,6 @@ public class UserHelper {
     private User userRoma = new User("Рома", "Виноградов", 30);
     private User userUra = new User("Юра", "Карп", 28);
 
-    private boolean isManAnya = false;
-    private boolean isManRoma = true;
-
     public void printUsers() {
         userAnya.printInfo();
         userAnya.printAdditionalInfo();
@@ -29,7 +26,6 @@ public class UserHelper {
     }
 
     public String getBossName(User user) {
-        Work work = user.getWork();
-        return work.getBoss();
+       return user.getBoss();
     }
 }
