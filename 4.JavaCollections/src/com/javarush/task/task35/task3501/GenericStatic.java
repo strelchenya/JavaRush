a169e4e5b38db2ebeecdcd82fe705e1e9f050dfa
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-25 16:03:06 +0300

    Add task 3501

--- 4.JavaCollections/src/com/javarush/task/task35/task3501/GenericStatic.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/GenericStatic.java
@@ -1,7 +1,7 @@
 package com.javarush.task.task35.task3501;
 
 public class GenericStatic <T extends Number>{
-    public static <T extends Number> T someStaticMethod(T genericObject) {
+    public static <T> T someStaticMethod(T genericObject) {
         System.out.println(genericObject);
         return genericObject;
     }
