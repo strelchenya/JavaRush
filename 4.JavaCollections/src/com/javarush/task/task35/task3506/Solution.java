f70e7f635d56225934961df9a17de69e9b265b37
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-26 01:34:15 +0300

    Add task 3506

--- 4.JavaCollections/src/com/javarush/task/task35/task3506/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3506/Solution.java
@@ -10,9 +10,9 @@ Wildcards
 
 public class Solution {
 
-    public static <D, H extends D, S extends H> void add(List<D> destinationList, List<S> sourceList) {
-        ListIterator<D> destListIterator = destinationList.listIterator();
-        ListIterator<S> srcListIterator = sourceList.listIterator();
+    public static <H> void add(List<? super H> destinationList, List<? extends H> sourceList) {
+        ListIterator<? super H> destListIterator = destinationList.listIterator();
+        ListIterator<? extends H> srcListIterator = sourceList.listIterator();
         for (int i = 0; i < sourceList.size(); i++) {
             destListIterator.add(srcListIterator.next());
         }
