1a8e10de195b5a617aeb8d14d8e004645250126c
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-25 22:15:05 +0300

    Add task 3505

--- 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task35.task3509;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 /* 
 Collections & Generics
@@ -9,20 +10,29 @@ Collections & Generics
 public class Solution {
 
     public static void main(String[] args) {
+        HashMap<Object, Number> maps = newHashMap(newArrayList("first", "second"), newArrayList(1,2));
+        System.out.println(maps.toString());
+
     }
 
-    public static ArrayList newArrayList(Object... elements) {
-        //напишите тут ваш код
-        return null
+    public static <T> ArrayList<T> newArrayList(T... elements) {
+        ArrayList <T> arrayList = new ArrayList<>(Arrays.asList(elements));
+        return arrayList;
     }
 
-    public static HashSet newHashSet(Object... elements) {
-        //напишите тут ваш код
-        return null
+    public static <T> HashSet <T> newHashSet(T... elements) {
+        HashSet <T> hashSet = new HashSet<>(Arrays.asList(elements));
+        return hashSet;
     }
 
-    public static HashMap newHashMap(List keys, List values) {
-        //напишите тут ваш код
-        return null;
+    public static <K, V> HashMap <K, V> newHashMap(List <? extends K> keys, List <? extends V> values) {
+        if (keys.size() != values.size()){
+            throw new IllegalArgumentException();
+        }
+        HashMap<K,V>  hashMap = new HashMap<>(values.size());
+        for (int i = 0; i < keys.size(); i++) {
+            hashMap.put(keys.get(i), values.get(i));
+        }
+        return hashMap;
     }
 }
