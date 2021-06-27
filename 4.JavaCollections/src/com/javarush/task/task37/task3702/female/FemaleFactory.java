598d18e819b65bb4089fba617f3c055c1095813b
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-27 20:36:00 +0300

    Add task 3702 Factory

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
@@ -1,8 +1,9 @@
 package com.javarush.task.task37.task3702.female;
 
+import com.javarush.task.task37.task3702.AbstractFactory;
 import com.javarush.task.task37.task3702.Human;
 
-public class FemaleFactory implements Human {
+public class FemaleFactory implements Human, AbstractFactory {
 
     public Human getPerson(int age) {
         if (age <= KidGirl.MAX_AGE) {
