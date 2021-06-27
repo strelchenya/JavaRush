d53d759e06f673bce817c0932491cbd83ecdd0e4
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-27 22:24:34 +0300

    Add task 3601 add classes

--- 4.JavaCollections/src/com/javarush/task/task36/task3601/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3601/Solution.java
@@ -9,27 +9,14 @@ MVC - простая версия
 
 public class Solution {
     public static void main(String[] args) {
-        new Solution().fireShowDataEvent();
+        new View().fireShowDataEvent();
     }
 
-    public List<String> getData() {
-        List<String> data = new ArrayList<String>() {{
-            add("First string");
-            add("Second string");
-            add("Third string");
-        }};
-        return data;
-    }
 
-    public void fireShowDataEvent() {
-        System.out.println(onShowDataList());
-    }
 
-    public List<String> getStringDataList() {
-        return getData();
-    }
 
-    public List<String> onShowDataList() {
-        return getStringDataList();
-    }
+
+
+
+
 }
