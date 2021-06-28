5bca5e2d02a1d4be0f5fa9038c6e35919094c4bb
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-28 23:31:31 +0300

    Add task 3605

--- 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task36.task3605;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.TreeSet;
 
 /* 
@@ -10,6 +13,35 @@ import java.util.TreeSet;
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+
+        TreeSet<String> treeSetCharacter = new TreeSet<>();
+
+        try (FileInputStream fis = new FileInputStream(args[0])) {
+
+            int current;
+            while (fis.available() > 0) {
+                current = fis.read();
+                if ((current >= 65 && current <= 90) || (current >= 97 && current <= 122)) {
+                    char symbol = (char) current;
+                    treeSetCharacter.add(String.valueOf(symbol).toLowerCase());
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int size = 5;
+        if (treeSetCharacter.size() < 5) {
+            size = treeSetCharacter.size();
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(treeSetCharacter.pollFirst());
+
+        }
     }
 }
