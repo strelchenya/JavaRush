d64ee04ebbffabd4c09f7dd880d2b9eee69d4884
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-29 14:28:47 +0300

    Add task 3612 add updateLastDate

--- 4.JavaCollections/src/com/javarush/task/task36/task3612/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3612/Solution.java
@@ -34,6 +34,10 @@ public class Solution {
     }
 
     protected void updateLastDate(long delta) {
+
+        dates.remove(lastDate);
         lastDate.setTime(lastDate.getTime() + delta);
+        dates.add(lastDate);
+
     }
 }
