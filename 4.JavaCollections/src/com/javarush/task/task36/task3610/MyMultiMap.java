a29d84bb805dd6923a2b363b07368f7d715b1a4a
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-06-30 19:04:35 +0300

    Add task 3610 MyMultiMap add method put, size and remove

--- 4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
@@ -15,37 +15,84 @@ public class MyMultiMap<K, V> extends HashMap<K, V> implements Cloneable, Serial
 
     @Override
     public int size() {
-        //напишите тут ваш код
+        int count = 0;
+        for (Map.Entry<K, List<V>> element: map.entrySet()) {
+            count += element.getValue().size();
+        }
+        return count;
     }
 
     @Override
     public V put(K key, V value) {
-        //напишите тут ваш код
+        List<V> keyList = null;
+        if (map.get(key) != null){
+            keyList = new ArrayList<>(map.get(key));
+            if (keyList.size() == repeatCount){
+                keyList.remove(0);
+            }
+            keyList.add(value);
+        }else {
+            keyList = new ArrayList<>();
+            keyList.add(value);
+        }
+
+        if (map.containsKey(key) && keyList.size() < repeatCount){
+
+            map.put(key, keyList);
+            return keyList.get(keyList.size()-2);
+        }
+        else if (keyList.size() == repeatCount){
+            map.put(key, keyList);
+            return keyList.get(keyList.size()-2);
+        }
+
+        map.put(key, keyList);
+        if (map.get(key).size()-1 == 0){
+            return null;
+        }
+       return  keyList.get(keyList.size()-2);
     }
 
     @Override
     public V remove(Object key) {
-        //напишите тут ваш код
+        V v = null;
+        if (map.containsKey(key) && map.get(key).size() !=0){
+            v = map.get(key).get(0);
+        }
+
+        if (!map.containsKey(key)){
+            return null;
+        }
+        if (map.get(key).size() > 1){
+            map.get(key).remove(0);
+            return v;
+        }
+        map.remove(key);
+        return null;
     }
 
     @Override
     public Set<K> keySet() {
-        //напишите тут ваш код
+        return null;
     }
 
     @Override
     public Collection<V> values() {
         //напишите тут ваш код
+        return  null;
     }
 
     @Override
     public boolean containsKey(Object key) {
+
         //напишите тут ваш код
+        return false;
     }
 
     @Override
     public boolean containsValue(Object value) {
         //напишите тут ваш код
+        return false;
     }
 
     @Override
