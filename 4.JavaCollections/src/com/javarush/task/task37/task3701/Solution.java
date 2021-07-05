a72560e583294d053d2041cd8245f45508fdbccd
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-06 02:08:01 +0300

    Add task 3701 add class RoundIterator

--- 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
@@ -27,6 +27,60 @@ public class Solution<T> extends ArrayList<T> {
         }
     }
 
-    public class RoundIterator {
+    @Override
+    public Iterator<T> iterator() {
+        return new RoundIterator();
+    }
+
+    public class RoundIterator implements Iterator<T> {
+        int cursor;       // index of next element to return
+        int lastRet = -1; // index of last element returned; -1 if no such
+        int expectedModCount = modCount;
+
+        public boolean hasNext() {
+            return size() > 0;
+        }
+
+        @SuppressWarnings("unchecked")
+        public T next() {
+            checkForComodification();
+            int i = cursor;
+            if (i >= size())
+                i = 0;
+            //private transient Object[] elementData;
+            Object[] elementData = null;
+            try {
+                Field elementDataField = ArrayList.class.getDeclaredField("elementData");
+                elementDataField.setAccessible(true);
+                elementData = (Object[]) elementDataField.get(Solution.this);
+
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+            }
+            if (i >= elementData.length)
+                throw new ConcurrentModificationException();
+            cursor = i + 1;
+            return (T) elementData[lastRet = i];
+        }
+
+        public void remove() {
+            if (lastRet < 0)
+                throw new IllegalStateException();
+            checkForComodification();
+
+            try {
+                Solution.this.remove(lastRet);
+                cursor = lastRet;
+                lastRet = -1;
+                expectedModCount = modCount;
+            } catch (IndexOutOfBoundsException ex) {
+                throw new ConcurrentModificationException();
+            }
+        }
+
+        final void checkForComodification() {
+            if (modCount != expectedModCount)
+                throw new ConcurrentModificationException();
+        }
+
     }
 }
