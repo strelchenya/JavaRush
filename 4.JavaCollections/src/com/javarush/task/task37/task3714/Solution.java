e43ea2844166fcce56b273060d931c481beff6ff
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-08 23:12:24 +0300

    Add task 3714 add method romanToInteger

--- 4.JavaCollections/src/com/javarush/task/task37/task3714/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3714/Solution.java
@@ -19,5 +19,24 @@ public class Solution {
     }
 
     public static int romanToInteger(String s) {
+        Map<Character, Integer> numbers = new HashMap<>();
+        numbers.put('I', 1);
+        numbers.put('V', 5);
+        numbers.put('X', 10);
+        numbers.put('L', 50);
+        numbers.put('C', 100);
+        numbers.put('D', 500);
+        numbers.put('M', 1000);
+
+        int sum = numbers.get(s.charAt(s.length() - 1));
+
+        for (int i = s.length() - 2; i >= 0; --i) {
+            if (numbers.get(s.charAt(i)) < numbers.get(s.charAt(i + 1))) {
+                sum -= numbers.get(s.charAt(i));
+            } else {
+                sum += numbers.get(s.charAt(i));
+            }
+        }
+        return sum;
     }
 }
