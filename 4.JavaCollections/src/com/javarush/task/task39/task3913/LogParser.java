383502722845f95c1382354b3238e3c70d92c679
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-13 23:30:32 +0300

    Add task 3913 LogParser_1

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
@@ -13,5 +13,35 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class LogParser {
+public class LogParser implements IPQuery{
+    private Path logDir;
+
+    public LogParser(Path logDir) {
+        this.logDir = logDir;
+    }
+
+    @Override
+    public int getNumberOfUniqueIPs(Date after, Date before) {
+        return 0;
+    }
+
+    @Override
+    public Set<String> getUniqueIPs(Date after, Date before) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPsForUser(String user, Date after, Date before) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPsForEvent(Event event, Date after, Date before) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPsForStatus(Status status, Date after, Date before) {
+        return null;
+    }
 }
