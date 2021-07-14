306b9fc0b5b5866169c789251942e62f57f4e6f0
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-14 22:20:09 +0300

    Add task 3803 methodThrowsClassCastException

--- 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
@@ -10,11 +10,12 @@ Runtime исключения (unchecked exception)
 
 public class VeryComplexClass {
     public void methodThrowsClassCastException() {
-
+        Object i = Integer.valueOf(42);
+        String s = (String)i;
     }
 
     public void methodThrowsNullPointerException() {
-
+        Integer error = 1 > 2 ? 3 : (4 > 5 ? new Integer(6) : null);
     }
 
     public static void main(String[] args) {
