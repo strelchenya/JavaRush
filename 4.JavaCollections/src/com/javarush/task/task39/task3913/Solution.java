6931f36426af975b108718777fa93297b8bc4b1a
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-14 15:46:54 +0300

    Add task 3913 LogParser_2

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -5,7 +5,13 @@ import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) {
-        LogParser logParser = new LogParser(Paths.get("c:/logs/"));
+        LogParser logParser = new LogParser(Paths.get("C:\\GitHub\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\task3913\\logs"));
+        System.out.println(logParser.getUniqueIPs(null, new Date()));
+        System.out.println(logParser.getUniqueIPs(null, null));
         System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
+        System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
+        System.out.println(logParser.getIPsForUser("Eduard Petrovich Morozko", null, null));
+        System.out.println(logParser.getIPsForEvent(Event.LOGIN, null, null));
+        System.out.println(logParser.getIPsForStatus(Status.OK, null, null));
     }
 }
