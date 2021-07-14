2b963d3cc22b77549737b36046a3c5b3cf59cedc
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-14 22:16:44 +0300

    Add task 3802 veryComplexMethod

--- 4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
@@ -4,9 +4,11 @@ package com.javarush.task.task38.task3802;
 Проверяемые исключения (checked exception)
 */
 
+import java.io.File;
+
 public class VeryComplexClass {
     public void veryComplexMethod() throws Exception {
-        //напишите тут ваш код
+        this.clone();
     }
 
     public static void main(String[] args) {
