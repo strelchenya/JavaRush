daa7e6c749f51782add670d4fd081d2d6e020aca
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-17 17:41:25 +0300

    Add task 3810 Changelog

--- 4.JavaCollections/src/com/javarush/task/task38/task3810/Changelog.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Changelog.java
@@ -1,8 +1,8 @@
 package com.javarush.task.task38.task3810;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
 
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Changelog {
-    //напиши свой код
+    Revision[] value() default {};
 }
--- 4.JavaCollections/src/com/javarush/task/task38/task3810/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Solution.java
@@ -6,9 +6,7 @@ package com.javarush.task.task38.task3810;
 
 @Changelog({
         @Revision(
-                revision = 4089,
-                date = @Date(year = 2011, month = 5, day = 30, hour = 18, minute = 35, second = 18),
-                comment = "Новый файл добавлен"),
+                revision = 4089, date = @Date(year = 2011, month = 5, day = 30, hour = 18, minute = 35, second = 18), comment = "Новый файл добавлен"),
         @Revision(
                 revision = 6018,
                 date = @Date(year = 2013, month = 1, day = 1, hour = 0, minute = 0, second = 1),
@@ -18,7 +16,7 @@ package com.javarush.task.task38.task3810;
                 revision = 10135,
                 date = @Date(year = 2014, month = 12, day = 31, hour = 23, minute = 59, second = 59),
                 authors = {@Author(value = "Диана", position = Position.OTHER),
-                        @Author("Игорь"),
+                        @Author(value = "Игорь"),
                         @Author(value = "Витек", position = Position.SENIOR)})
 })
 public class Solution {
