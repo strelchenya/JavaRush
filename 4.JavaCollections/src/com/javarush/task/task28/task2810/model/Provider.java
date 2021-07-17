b548359d9375aef70bfe340f80a06e5d4e651aa7
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-17 18:29:06 +0300

    Add task 2810 Aggregator_1

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task28.task2810.model;
+
+public class Provider {
+    private Strategy strategy;
+
+    public Provider(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public Strategy getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+}
