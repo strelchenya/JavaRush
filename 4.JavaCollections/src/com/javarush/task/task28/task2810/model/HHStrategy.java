998283c319b0dc3e838ac2a711666d98870f07cd
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-18 14:19:05 +0300

    Add task 2810 Aggregator_4

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,4 +1,6 @@
 package com.javarush.task.task28.task2810.model;
 
 public class HHStrategy implements Strategy{
+
+    final static private String URL_FORMAT = "https://grc.ua/search/vacancy?text=java+%s&page=%d";
 }
