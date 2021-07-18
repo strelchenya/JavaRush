0f1db93d3a15fceb94fa073482d97e77721caa42
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-19 01:24:52 +0300

    Add task 3903 swapBits

--- 4.JavaCollections/src/com/javarush/task/task39/task3903/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3903/Solution.java
@@ -24,6 +24,10 @@ public class Solution {
     }
 
     public static long swapBits(long number, int i, int j) {
+        if (((number >>> i) & 1) != ((number >>> j) & 1)) {
+            long bitMask = (1L << i) | (1L << j);
+            number ^= bitMask;
+        }
         return number;
     }
 }
