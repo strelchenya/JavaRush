3aff1c9347cbfe40ac39f389e0863787d48c367b
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-08-13 23:03:21 +0300

    Add task 3912

--- 4.JavaCollections/src/com/javarush/task/task39/task3912/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3912/Solution.java
@@ -6,22 +6,10 @@ package com.javarush.task.task39.task3912;
 
 public class Solution {
     public static void main(String[] args) {
-        int[][] matrix = new int[][]{
-                { 1, 1, 1, 1, 0, 1, 0, 0, 1, 1 },
-                { 1, 0, 1, 1, 0, 1, 0, 0, 1, 1 },
-                { 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
-                { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1 },
-                { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 0, 0, 0, 1, 1, 1, 1, 1, 1 },
-                { 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 0, 0, 0, 0, 1, 0, 1, 1, 1 }
-        };
-        System.out.println(maxSquare(matrix));
+
     }
 
     public static int maxSquare(int[][] matrix) {
-        return 0;
+
     }
 }
