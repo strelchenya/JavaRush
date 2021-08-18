67962eec039062f60e68f1413ca38a94800db6ba
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-08-18 17:27:02 +0300

    Add task 2601

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
@@ -1,8 +1,7 @@
 package com.javarush.task.task26.task2601;
 
-/*
-Почитать в инете про медиану выборки
-*/
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Solution {
 
@@ -11,7 +10,30 @@ public class Solution {
     }
 
     public static Integer[] sort(Integer[] array) {
-        //implement logic here
+        final double mediana = getMediana(array);
+
+        Arrays.sort(array, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                double v1 = o1.intValue() - mediana;
+                double v2 = o2.intValue() - mediana;
+                return (int) ((v1 * v1 - v2 * v2) * 100);
+            }
+        });
+
         return array;
+    }
+
+    private static double getMediana(Integer[] array) {
+        Arrays.sort(array);
+        double res;
+        int length = array.length;
+
+        if (length % 2 == 1) {
+            res = array[length / 2];
+        } else {
+            res = (array[length / 2 - 1] + array[length / 2]) / 2.;
+        }
+        return res;
     }
 }
