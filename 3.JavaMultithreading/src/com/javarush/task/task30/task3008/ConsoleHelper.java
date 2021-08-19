342babe440903f300d540ba4a7611ce370196a61
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-08-19 19:43:06 +0300

    Add task 3008 Chat_2

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,48 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+
+        String line = "";
+
+        boolean read = true;
+
+        while (read) {
+            try {
+                line = reader.readLine();
+                read = false;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return line;
+    }
+
+
+    public static int readInt() {
+        boolean read = true;
+        int parseToInt = 0;
+
+        while (read) {
+            try {
+                parseToInt = Integer.parseInt(readString());
+                read = false;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+
+        return parseToInt;
+    }
 }
