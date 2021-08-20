ed6660246b9e9e00768b9468efb788350d3a3c51
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-08-20 19:01:37 +0300

    Add task 3008 Chat_13

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,11 +1,49 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 public class Client {
 
     protected Connection connection;
-    private volatile boolean clientConnected = false;
+    private volatile boolean clientConnected;
+
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Введите адрес сервера:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Введите ваше имя:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Сообщение не доставлено");
+            clientConnected = false;
+        }
+    }
 
     public class SocketThread extends Thread{
 
