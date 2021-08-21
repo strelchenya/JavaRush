3596b9b639e67709205706e69172e6aff8627b0b
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-08-21 23:53:37 +0300

    Add task 3110 Archiver_8

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,34 +1,58 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
 
 public class Archiver {
     public static void main(String[] args) {
 // C:\Users\Strelchenya\Desktop\1\zip.zip
 // C:\Users\Strelchenya\Desktop\1\all.pdf
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            String pathZipFile = reader.readLine();
-            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathZipFile));
 
-            String pathFile = reader.readLine();
-            zipFileManager.createZip(Paths.get(pathFile));
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception exception) {
-            exception.printStackTrace();
+        Operation operation = null;
+
+        while (operation != Operation.EXIT){
+            try {
+                operation = askOperation();
+                if (operation != null){
+                    CommandExecutor.execute(operation);
+                }
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception exception) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
         }
 
-        ExitCommand exitCommand = new ExitCommand();
-        try {
-            exitCommand.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
+    }
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                Operation.CREATE.ordinal() + " - упаковать файлы в архив\n" +
+                Operation.ADD.ordinal() + " - добавить файл в архив\n" +
+                Operation.REMOVE.ordinal() + " - удалить файл из архива\n" +
+                Operation.EXTRACT.ordinal() + " - распаковать архив\n" +
+                Operation.CONTENT.ordinal() + " - просмотреть содержимое архива\n" +
+                Operation.EXIT.ordinal() + " - выход");
+
+        int numberOperation = ConsoleHelper.readInt();
+
+        switch (numberOperation) {
+            case 0:
+                return Operation.CREATE;
+            case 1:
+                return Operation.ADD;
+            case 2:
+                return Operation.REMOVE;
+            case 3:
+                return Operation.EXTRACT;
+            case 4:
+                return Operation.CONTENT;
+            case 5:
+                return Operation.EXIT;
+            default:
+                throw new IllegalArgumentException();
         }
     }
+
 }
