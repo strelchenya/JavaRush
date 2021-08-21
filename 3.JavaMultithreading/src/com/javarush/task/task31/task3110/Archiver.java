9d4fdf0556a6e1ca762fe6e8b47d9e04cda2bf1b
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-08-21 22:27:11 +0300

    Add task 3110 Archiver_4

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,6 +22,13 @@ public class Archiver {
             e.printStackTrace();
         } catch (Exception exception) {
             exception.printStackTrace();
+        }
+
+        ExitCommand exitCommand = new ExitCommand();
+        try {
+            exitCommand.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/Command.java
@@ -0,0 +1,6 @@
+package com.javarush.task.task31.task3110.command;
+
+public interface Command {
+
+    public void execute() throws Exception;
+}
