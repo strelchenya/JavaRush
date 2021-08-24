752a452cf338531a175ba22184d10ccb4e334045
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-08-24 23:10:37 +0300

    Add task 2803

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2803/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task28.task2803;
+
+/*
+ThreadLocalRandom
+*/
+
+public class Solution {
+    public static int getRandomIntegerBetweenNumbers(int from, int to) {
+        return 0;
+    }
+
+    public static double getRandomDouble() {
+        return 0;
+    }
+
+    public static long getRandomLongBetween0AndN(long n) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+    }
+}
