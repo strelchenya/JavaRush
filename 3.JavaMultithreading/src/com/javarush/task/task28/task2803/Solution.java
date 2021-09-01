6c5e89ab42314d76e9c4c53e31fc78fa568c7670
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-09-01 22:56:54 +0300

    Add task 2803 ThreadLocalRandom

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2803/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2803/Solution.java
@@ -4,17 +4,19 @@ package com.javarush.task.task28.task2803;
 ThreadLocalRandom
 */
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Solution {
     public static int getRandomIntegerBetweenNumbers(int from, int to) {
-        return 0;
+        return ThreadLocalRandom.current().nextInt(from, to + 1);
     }
 
     public static double getRandomDouble() {
-        return 0;
+        return ThreadLocalRandom.current().nextDouble();
     }
 
     public static long getRandomLongBetween0AndN(long n) {
-        return 0;
+        return ThreadLocalRandom.current().nextLong(n + 1);
     }
 
     public static void main(String[] args) {
