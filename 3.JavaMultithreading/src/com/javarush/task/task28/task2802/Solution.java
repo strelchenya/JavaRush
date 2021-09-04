c531e62eee22b733c89176f2d48aca420708db47
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-09-04 22:33:07 +0300

    Add task 2802 ThreadFactory

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2802/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2802/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task28.task2802;
 
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /* 
 Пишем свою ThreadFactory
@@ -39,10 +40,28 @@ public class Solution {
         factory.newThread(r).start();
     }
 
-    public static class AmigoThreadFactory implements ThreadFactory{
-        @Override
+    public static class AmigoThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        public AmigoThreadFactory() {
+            group = Thread.currentThread().getThreadGroup();
+            namePrefix = group.getName() + "-pool-" +
+                    poolNumber.getAndIncrement() +
+                    "-thread-";
+        }
+
         public Thread newThread(Runnable r) {
-            return null;
+            Thread t = new Thread(group, r,
+                    namePrefix + threadNumber.getAndIncrement(),
+                    0);
+            if (t.isDaemon())
+                t.setDaemon(false);
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+            return t;
         }
     }
 }
