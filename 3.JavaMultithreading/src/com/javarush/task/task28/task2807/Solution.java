8130820031473eff939de34771cb57a0b40cc584
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-09-11 13:19:06 +0300

    Add task 2807 LinkedBlockingQueue

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2807/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2807/Solution.java
@@ -1,25 +1,22 @@
 package com.javarush.task.task28.task2807;
 
-/*
-Знакомство с ThreadPoolExecutor
-*/
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
-        // Add your code here
+        LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+        for (int i = 1; i <= 10; i++) {
+            final int localId = i;
+            queue.add(() -> doExpensiveOperation(localId));
+        }
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(3, 5, 1000, TimeUnit.MILLISECONDS, queue);
+        executor.prestartAllCoreThreads();
 
-        /* Example output
-pool-1-thread-2, localId=2
-pool-1-thread-3, localId=3
-pool-1-thread-1, localId=1
-pool-1-thread-3, localId=5
-pool-1-thread-2, localId=4
-pool-1-thread-3, localId=7
-pool-1-thread-1, localId=6
-pool-1-thread-3, localId=9
-pool-1-thread-2, localId=8
-pool-1-thread-1, localId=10
-         */
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
     }
 
     private static void doExpensiveOperation(int localId) {
